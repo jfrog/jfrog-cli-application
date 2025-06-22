059415f3cf79f2454ad879d2908c1066a2e6a8e1
delarea <eyalde@jfrog.com>
2025-06-22 14:55:36 +0300

    CR

--- apptrust/commands/version/release_app_version_cmd_test.go
+++ apptrust/commands/version/release_app_version_cmd_test.go
@@ -4,7 +4,6 @@ import (
 	"errors"
 	"testing"
 
-	"github.com/jfrog/jfrog-cli-application/apptrust/commands"
 	mockversions "github.com/jfrog/jfrog-cli-application/apptrust/service/versions/mocks"
 	"go.uber.org/mock/gomock"
 
@@ -93,26 +92,4 @@ func TestReleaseAppVersionCommand_Run_Error(t *testing.T) {
 	err := cmd.Run()
 	assert.Error(t, err)
 	assert.Contains(t, err.Error(), "service error occurred")
-}
-
-func TestReleaseAppVersionCommand_ServerDetails(t *testing.T) {
-	ctrl := gomock.NewController(t)
-	defer ctrl.Finish()
-
-	serverDetails := &config.ServerDetails{}
-	cmd := &releaseAppVersionCommand{
-		serverDetails: serverDetails,
-	}
-
-	details, err := cmd.ServerDetails()
-	assert.NoError(t, err)
-	assert.Equal(t, serverDetails, details)
-}
-
-func TestReleaseAppVersionCommand_CommandName(t *testing.T) {
-	ctrl := gomock.NewController(t)
-	defer ctrl.Finish()
-
-	cmd := &releaseAppVersionCommand{}
-	assert.Equal(t, commands.VersionRelease, cmd.CommandName())
 }
